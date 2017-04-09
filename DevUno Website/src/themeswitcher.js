0c0d9d14b39065b3d16dcefa26946dbea3e83859
Cameron Rasmussen <cameron.p.rasmussen@gmail.com>
2017-04-09 01:54:41 -0600

    Starting
    Starting a JS button reference to CSS events.
       Used to darken the theme of the overall page.

---
+++ DevUno Website/src/themeswitcher.js
@@ -0,0 +1,6 @@
+var headerColor = document.getElementById('mainHeader');
+var buttonChange = document.getElementById('themeButton')
+
+function changeHeaderColor() {
+    headerColor.style.backgroundColor = rgb(249, 89, 82);
+}
