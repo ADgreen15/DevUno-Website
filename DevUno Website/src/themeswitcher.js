f1f4d5920a6c361081a2ff20e3ccc2dd27f099d4
Cameron Rasmussen <cameron.p.rasmussen@gmail.com>
2017-04-09 17:45:47 -0600

    Functional Commit
    Will turn the theme dark if you press the button. Need to add
    functionality to switch back and forth between dark and light.

--- DevUno Website/src/themeswitcher.js
+++ DevUno Website/src/themeswitcher.js
@@ -1,6 +1,21 @@
-var headerColor = document.getElementById('mainHeader');
-var buttonChange = document.getElementById('themeButton')
+function themeSwitch(){
 
-function changeHeaderColor() {
-    headerColor.style.backgroundColor = rgb(249, 89, 82);
+//changing header background color
+    document.getElementById('mainHeader').style.backgroundColor = '#202429';
+
+//changing hr color
+    document.getElementById('hrBreak').style.borderTopColor = '#323232';
+
+//changing the color of the links when pressed
+    document.getElementById('homeLink').style.color = '#e0f2f2';
+    document.getElementById('webdevLink').style.color = '#e0f2f2';
+    document.getElementById('graphicdesignLink').style.color = '#e0f2f2';
+    document.getElementById('aboutLink').style.color = '#e0f2f2';
+    document.getElementById('contactLink').style.color = '#e0f2f2';
+
+//changing logo opacity
+    document.getElementById('headerLogoLight').style.opacity = '0';
+    document.getElementById('headerLogoDark').style.opacity = '100';
+
+//add elements that need theme changing here
 }
